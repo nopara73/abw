ca27b8ebec8928b4936781834175996c57b82228
molnard <molnardavid84@gmail.com>
2019-08-12 12:31:02 +0200

    Add pw compatibility.

---
+++ WalletWasabi/Helpers/PasswordHelper.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace WalletWasabi.Helpers
+{
+	public static class PasswordHelper
+	{
+		public static string[] GetPossiblePasswords(string originalPassword)
+		{
+			List<string> possiblePasswords = new List<string>()
+			{
+				originalPassword,
+				StringCutIssue(originalPassword)
+			};
+
+			return possiblePasswords.ToArray();
+		}
+
+		private static string StringCutIssue(string text)
+		{
+			// On OSX Avalonia gets the string from the Clipboard as byte[] and size.
+			// The size was mistakenly taken from the size of the original string which is not correct because of the UTF8 encoding.
+
+			byte[] bytes = Encoding.Default.GetBytes(text);
+			var myString = Encoding.UTF8.GetString(bytes.Take(text.Length).ToArray());
+			return text.Substring(0, myString.Length);
+		}
+	}
+}
